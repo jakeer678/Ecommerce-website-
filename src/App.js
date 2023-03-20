1903d0fa6f8a6dc1f1683d0db78df2869f178a0a
jakeer678 <jakeersasanoor@gmail.com>
2023-03-20 18:36:35 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+
+
+
+function App() {
+  
+
+
+  return (
+    <React.Fragment>
+     <Header />
+     <ProductList />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/products.js
@@ -0,0 +1,37 @@
+
+
+export const products = [
+  {
+    title: "Colors",
+
+    price: 100,
+
+   
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  },
+
+  {
+    title: "Black Colors",
+
+    price: 50,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  },
+
+  {
+    title: "Yellow Colors",
+
+    price: 70,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  },
+
+  {
+    title: "Blue Color",
+
+    price: 100,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+  },
+];
