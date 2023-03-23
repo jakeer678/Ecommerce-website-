2bf26d917cfcdf9c7e6b561b41009164c45689b8
jakeer678 <jakeersasanoor@gmail.com>
2023-03-23 10:18:09 +0530

    updated

--- src/components/productList/ProductPage.js
+++ src/components/productList/ProductPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { products } from "./products";
+import './ProductPage.css'
+
 
 const ProductPage = () => {
   const params = useParams();
@@ -13,22 +15,9 @@ const ProductPage = () => {
         <h4>Product Detail</h4>
         <p>{productDeatail?.title}</p>
         <div className="routespage">
-        <img src={productDeatail.imageUrl} alt="img" />
-        <p>Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of
-          sorrows, hates no prosecutors will unfold in the enduring of which
-          were born in it? Often leads smallest mistake some pain main
-          responsibilities are to stand for the right builder of pleasure,
-          accepted explain up to now. , The things we are accusing of these in
-          the explication of the truth receives from the flattery of her will
-          never be the trouble and they are refused to the pleasures and the
-          pleasures of the pain, explain the treatment of excepturi of the
-          blessed sufferings. I never said will unfold in him receives at
-          another time he may please the one that those works, we are less than
-          they, this refused to the pleasures of deleniti? Those are! Will
-          </p>
+          <img src={productDeatail.imageUrl} alt="img" />
+          <p>{productDeatail.description}</p>
         </div>
-        
-      
       </section>
     </div>
   );
--- src/components/productList/products.js
+++ src/components/productList/products.js
@@ -6,6 +6,8 @@ export const products = [
     price: 100,
 
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    description: "1080P FHD action camera kit 170 degree angle with 2.0 inch LCD screen Built-in 3.7V 900mAh battery Motion Detection Anti-shake Loop recording Time-lapse, burst, continuous shooting 30m waterproof Video resolution: 1080P at 30fps, 720P at 60fps, VGA at 30fps Charging time: About 3.5 hours Working time: About 1.5 hours at 1080P 30fps Image resolution: 12M, 10M, 8M, 5M, 3M, 2MHD, 1.3M, iOS 8, Android 4X quad-core devices Package includes W9C action camera with waterproof housing, waterproof housing back door, short connector + short screw, long connector + short screw, quick release buckle base, quick clip, quick clip frame base, handlebar holder, adapter, J-shaped quick release base, velcro belt, metal cable tie, 3M sticker, cleaning cloth, fixed band, USB cable, power adapter, English instruction",
+
   },
 
   {
@@ -15,6 +17,8 @@ export const products = [
     price: 50,
 
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    description: "1080P FHD action camera kit 170 degree angle with 2.0 inch LCD screen Built-in 3.7V 900mAh battery Motion Detection Anti-shake Loop recording Time-lapse, burst, continuous shooting 30m waterproof Video resolution: 1080P at 30fps, 720P at 60fps, VGA at 30fps Charging time: About 3.5 hours Working time: About 1.5 hours at 1080P 30fps Image resolution: 12M, 10M, 8M, 5M, 3M, 2MHD, 1.3M, iOS 8, Android 4X quad-core devices Package includes W9C action camera with waterproof housing, waterproof housing back door, short connector + short screw, long connector + short screw, quick release buckle base, quick clip, quick clip frame base, handlebar holder, adapter, J-shaped quick release base, velcro belt, metal cable tie, 3M sticker, cleaning cloth, fixed band, USB cable, power adapter, English instruction",
+
   },
 
   {
@@ -22,6 +26,7 @@ export const products = [
     title: "Yellow Colors",
 
     price: 70,
+    description: "1080P FHD action camera kit 170 degree angle with 2.0 inch LCD screen Built-in 3.7V 900mAh battery Motion Detection Anti-shake Loop recording Time-lapse, burst, continuous shooting 30m waterproof Video resolution: 1080P at 30fps, 720P at 60fps, VGA at 30fps Charging time: About 3.5 hours Working time: About 1.5 hours at 1080P 30fps Image resolution: 12M, 10M, 8M, 5M, 3M, 2MHD, 1.3M, iOS 8, Android 4X quad-core devices Package includes W9C action camera with waterproof housing, waterproof housing back door, short connector + short screw, long connector + short screw, quick release buckle base, quick clip, quick clip frame base, handlebar holder, adapter, J-shaped quick release base, velcro belt, metal cable tie, 3M sticker, cleaning cloth, fixed band, USB cable, power adapter, English instruction",
 
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
   },
@@ -31,6 +36,7 @@ export const products = [
     title: "Blue Color",
 
     price: 100,
+    description: "1080P FHD action camera kit 170 degree angle with 2.0 inch LCD screen Built-in 3.7V 900mAh battery Motion Detection Anti-shake Loop recording Time-lapse, burst, continuous shooting 30m waterproof Video resolution: 1080P at 30fps, 720P at 60fps, VGA at 30fps Charging time: About 3.5 hours Working time: About 1.5 hours at 1080P 30fps Image resolution: 12M, 10M, 8M, 5M, 3M, 2MHD, 1.3M, iOS 8, Android 4X quad-core devices Package includes W9C action camera with waterproof housing, waterproof housing back door, short connector + short screw, long connector + short screw, quick release buckle base, quick clip, quick clip frame base, handlebar holder, adapter, J-shaped quick release base, velcro belt, metal cable tie, 3M sticker, cleaning cloth, fixed band, USB cable, power adapter, English instruction",
 
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
   },
